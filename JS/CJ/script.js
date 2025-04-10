d3518032dd7fdc60d6914b7d158d91cedd19ca70
DanBeck-Dev <122324905+DanBeck-Dev@users.noreply.github.com>
2025-04-10 10:06:01 -0300

    Atualizando script.js

--- JS/CJ/script.js
+++ JS/CJ/script.js
@@ -8,18 +8,35 @@ function exibir(){
     const saudacao = document.createElement("h2");
     saudacao.textContent = "Bom dia, " + nome + "!";
     document.getElementById("formulario").replaceWith(saudacao);
-
 }
 
-function dark(){
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
+
+function tema(){
+    const corpo = document.body;
+    let corFundoAtual = getComputedStyle(corpo).backgroundColor;
+    if(corFundoAtual == 'rgb(0, 0, 0)'){
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.getElementById("tema").style.color = "black"
+    }
+    else{
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        document.getElementById("tema").style.color = "white"
+    }
 }
 
-function light(){
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
+document.getElementById("tema").addEventListener("click", tema);
+
+function aumentarFonte(){
+    let corpo = document.body;
+    let tamanhofonte = getComputedStyle(corpo).fontSize;
+    let tamanhoNumero = parseFloat(tamanhofonte);
+    tamanhoNumero += 2;
+    corpo.style.fontSize = tamanhoNumero + 'px';
 }
 
-document.getElementById("dark").addEventListener("click", dark);
-document.getElementById("light").addEventListener("click", light);
+let elementosFonte = document.getElementsByClassName("fonte");
+for (let i = 0; i < elementosFonte.length; i++) {
+    elementosFonte[i].addEventListener("click", aumentarFonte);
+}
