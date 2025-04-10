2fcb49faee7e2ac2555a05ea6a9cc42eb9870aa0
DanBeck-Dev <danilocunhavb@gmail.com>
2025-04-09 22:23:32 -0300

    Adição botões dark e light

--- JS/CJ/script.js
+++ JS/CJ/script.js
@@ -4,5 +4,22 @@ document.getElementById('title').innerText = 'Teste de JavaScript';
 
 function exibir(){
     let nome = document.getElementById("nome").value;
-    document.write("Bom Dia " + nome + "!!");
+    
+    const saudacao = document.createElement("h2");
+    saudacao.textContent = "Bom dia, " + nome + "!";
+    document.getElementById("formulario").replaceWith(saudacao);
+
 }
+
+function dark(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function light(){
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
+
+document.getElementById("dark").addEventListener("click", dark);
+document.getElementById("light").addEventListener("click", light);
